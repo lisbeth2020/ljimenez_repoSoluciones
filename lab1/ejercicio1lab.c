27a8f6acd032639ca71996a85550a02c9c889fdc
lisbeth2020 <ljimenez18@alumnos.utalca.cl>
2018-04-19 15:21:32 -0300

    se ha agregado laboratorio 1

---
+++ lab1/ejercicio1lab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+	int candidato1,candidato2,candidato3,v;
+	
+	
+	printf ("candidato 1: Javier Lueiza");
+	printf ("\ncandidato 2: Daniel Monsalve");
+	printf ("\ncandidato 3: Marcos Ramirez");
+	printf ("Para votar ingrese 1,2 o 3");
+	printf ("Ingrese su voto:");
+	scanf ("%d",&v);
+	while (!=f);
+
+	return 0;
+	
+	
+}
