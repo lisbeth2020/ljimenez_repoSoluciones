9cc0b2fce694491b1962be8583ca7f96a084c5f1
lisbeth2020 <smile_lisbeth14@hotmail.com>
2018-05-03 18:29:41 -0300

    ejercicio 2

---
+++ Lab4/ejerc2_lab4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+	int ju, ma,cda_uno,juan,manuel;
+	
+	cda_uno= 8/3;
+	ju= 5-cda_uno;
+	ma= 3-cda_uno;
+	 
+	juan= ju*10;
+	manuel= ma*10; 
+	 
+	printf ("Antonio debe pagar a juan:%d\n",juan);
+	printf ("Antonio debe pagar a manuel:%d",manuel);
+	 
+	 
+	return 0;	
+}
